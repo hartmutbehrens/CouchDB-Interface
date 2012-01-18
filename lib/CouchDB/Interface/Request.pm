89a610fbe34ad8c144efc71ad2ed3eff79a22c23
Hartmut Behrens <hartmut.behrens@gmail.com>
2012-01-18 14:49:31 +0200

    improve readability

--- lib/CouchDB/Interface/Request.pm
+++ lib/CouchDB/Interface/Request.pm
@@ -18,7 +18,7 @@ has uri => ( is => 'rw', required => 1 );
 has method => ( is => 'rw', required => 1 );
 has content => ( is => 'rw' );
 has debug => (is => 'rw', default => sub { return 0} );
-has ua => ( is => 'rw', default => sub { return Mojo::UserAgent->new->detect_proxy->connect_timeout(5) } );
+has ua => ( is => 'rw', default => sub { return Mojo::UserAgent->new->detect_proxy->connect_timeout( 5 ) } );
 has headers => (is => 'rw', default => sub { return { 'Cache-Control' => 'no-cache' } } );
 has json => ( is => 'rw', default => sub { return Mojo::JSON->new } );
 has max_retry => ( is => 'rw', default => sub { return 2 } );
@@ -26,25 +26,24 @@ has max_retry => ( is => 'rw', default => sub { return 2 } );
 
 sub execute {
 	my $self = shift;
-	my ($count,$response) = (0,undef);
-	my $method = $self->method;
-	my $content = $self->content;
-	 
+	
+	my $content = $self->content; 
 	if (ref $content) {	
         $content = $self->json->encode($content);
         $self->headers({ 'Cache-Control' => 'no-cache', 'Content-Type' => 'application/json' });
     }
-	
+    
+	my ($count,$response,$method) = (0,undef,$self->method);
 	while ( $response = $self->ua->$method($self->uri => $self->headers => $content )->res ) {
 		$count++;
 		say "Request repeat $count" if $count > 1 && $self->debug;
 		last if (defined $response->code) || ($count > $self->max_retry);
 	}
-	$self->describe($response) if $self->debug;
+	$self->_describe($response) if $self->debug;
 	return $response;
 }
 
-sub describe {
+sub _describe {
 	my ($self,$response) = @_;
 	say "\tRequest:  ", uc($self->method)," ",$self->uri;
 	say "\tSent Content:  ", $self->json->encode($self->content);
